6719adf988120835e1ef15df9cc658fba493de7d
Diogo <112704522+diogofacul@users.noreply.github.com>
2024-10-04 08:49:08 -0300

    Projeto base.

---
+++ IFPizza/src/main/java/ifsp/edu/IFPizza/IfPizzaApplication.java
@@ -0,0 +1,13 @@
+package ifsp.edu.IFPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfPizzaApplication.class, args);
+	}
+
+}
---
+++ IFPizza/src/test/java/ifsp/edu/IFPizza/IfPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.edu.IFPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
