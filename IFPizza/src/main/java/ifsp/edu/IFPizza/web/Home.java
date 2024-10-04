7a91520f720a3126d4072c21ec9c6a753226b6e0
Diogo <112704522+diogofacul@users.noreply.github.com>
2024-10-04 09:27:32 -0300

    Projeto funcionando base.

---
+++ IFPizza/src/main/java/ifsp/edu/IFPizza/web/Home.java
@@ -0,0 +1,12 @@
+package ifsp.edu.IFPizza.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+	@GetMapping("/")
+	public String Home() {
+		return "Home";
+	}
+}
